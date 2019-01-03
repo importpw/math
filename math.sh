83626cfc4a0822ae6f9fa738c9d53fdfa3d5ef09
Nathan Rajlich <nathan@tootallnate.net>
2019-01-02 21:49:11 -0800

    Add `math_random`

--- math.sh
+++ math.sh
@@ -6,3 +6,8 @@ math_min() {
 math_max() {
   printf "%s\n" "$@" | sort -gr | head -n1
 }
+
+# Credit: https://unix.stackexchange.com/a/424760/102771
+math_random() {
+  awk -v seed="$RANDOM" 'BEGIN { srand(seed); printf("%.4f\n", rand()) }'
+}
